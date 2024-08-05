4607297150a231a6975613589128e9e3a8ed3fff
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2024-08-05 08:26:02 +0800

    minor change to Profile page

--- app/(main)/profile/_layout.tsx
+++ app/(main)/profile/_layout.tsx
@@ -1,9 +1,8 @@
-import { Stack, Tabs } from 'expo-router';
+import { Slot, Stack, Tabs } from 'expo-router';
 import React from 'react';
 
 export default function ProfileLayout() {
   return (
-    <Stack>
-    </Stack>
+    <Slot/>
   );
 }

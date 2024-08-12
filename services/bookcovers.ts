17a9d3b7f2ac4ae13618ec847f7a98667abf3c6c
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2024-08-12 08:08:46 +0800

    updated bookcovers.ts

--- services/bookcovers.ts
+++ services/bookcovers.ts
@@ -4,5 +4,16 @@ export const BOOK_COVERS = [
     require("@/assets/bookcovers/2.jpg"),
     require("@/assets/bookcovers/3.jpg"),
     require("@/assets/bookcovers/4.jpg"),
-    require("@/assets/bookcovers/5.jpg")
+    require("@/assets/bookcovers/5.jpg"),
+    require("@/assets/bookcovers/6.jpg"),
+    require("@/assets/bookcovers/7.jpg"),
+    require("@/assets/bookcovers/8.jpg"),
+    require("@/assets/bookcovers/9.jpg"),
+    require("@/assets/bookcovers/10.jpg"),
+    require("@/assets/bookcovers/11.jpg"),
+    require("@/assets/bookcovers/12.jpg"),
+    require("@/assets/bookcovers/13.jpg"),
+    require("@/assets/bookcovers/14.jpg"),
+    require("@/assets/bookcovers/15.jpg"),
+
 ]
